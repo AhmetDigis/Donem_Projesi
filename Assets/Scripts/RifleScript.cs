db5a46eb9d9f09cc808fe798376d76e40824111f
AhmetDigis <101000191+AhmetDigis@users.noreply.github.com>
2022-04-14 23:30:05 +0300

    rifle script devam edildi. ses ve delik eklendi

--- Assets/Scripts/RifleScript.cs
+++ Assets/Scripts/RifleScript.cs
@@ -6,8 +6,9 @@ public class RifleScript : MonoBehaviour
 {
     [Header("Settings")]
     float feverFrequency_1;
-    float feverFrequency_2;
+    public float feverFrequency_2;
     public float range;
+
     [Header("Settings")]
 
     public AudioSource[] sounds;
@@ -22,25 +23,36 @@ public class RifleScript : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKey(KeyCode.Mouse0)){
 
-            rifleFire();
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Time.time > feverFrequency_1){
+                feverFrequency_1 = Time.time + feverFrequency_2;
+                rifleFire();
+            }
+                       
         }
 
     }
 
 
-    void rifleFire(){
+    void rifleFire()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(MyCamera.transform.position,MyCamera.transform.forward, out hit, range)){
+        if (Physics.Raycast(MyCamera.transform.position, MyCamera.transform.forward, out hit, range))
+        {
+
+            effects[0].Play();
+            sounds[0].Play();
+            Instantiate(effects[1], hit.point, Quaternion.LookRotation(hit.normal));
             Debug.Log(hit.transform.gameObject.name);
+
         }
 
 
