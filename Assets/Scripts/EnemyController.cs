93167091fbb510c39363692eb1f5c299db9f7bfd
AhmetDigis <101000191+AhmetDigis@users.noreply.github.com>
2022-04-19 23:22:24 +0300

    enemy animasyonlarına devam edildi

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,9 +7,13 @@ public class EnemyController : MonoBehaviour
 {
     NavMeshAgent navMesh;
     Animator enemyAnimator;
-    public GameObject target;
+    GameObject target;
+    float fireDistance = 7;
+    float SuspectDistance = 10;
 
-    float distance=10;
+    Vector3 startingPoint;
+    bool isFire = false;
+    bool isSuspect = false;
 
 
     void Start()
@@ -17,35 +21,94 @@ public class EnemyController : MonoBehaviour
         navMesh = GetComponent<NavMeshAgent>();
         enemyAnimator = GetComponent<Animator>();
         //enemyAnimator.SetBool("walk", true);
+        startingPoint = transform.position;
     }
 
 
     void LateUpdate()
     {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, distance);
-        float nearestDistance = Mathf.Infinity;
-        GameObject nearestTarget = null;
+        SuspectRange();
+        FiringRange();
+
+    }
+
+    void FiringRange()
+    {
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, fireDistance);
+
 
         foreach (var otherObject in hitColliders)
         {
 
             if (otherObject.gameObject.CompareTag("Player"))
             {
-
-                enemyAnimator.SetBool("walk", true);
-                navMesh.SetDestination(otherObject.transform.position);
+                enemyAnimator.SetBool("walk", false);
+                navMesh.isStopped=true;
+                enemyAnimator.SetBool("fireIdle", true);
 
             }
-
+            else
+            {               
+                enemyAnimator.SetBool("fireIdle", false);
+                
+            }
 
         }
 
 
     }
 
-    private void OnDrawGizmos() {
-        Gizmos.color=Color.red;
-        Gizmos.DrawWireSphere(transform.position, distance);
+
+    void SuspectRange()
+    {
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, SuspectDistance);
+
+
+        foreach (var otherObject in hitColliders)
+        {
+
+            if (otherObject.gameObject.CompareTag("Player"))
+            {
+                isSuspect = true;
+                enemyAnimator.SetBool("walk", true);
+                target = otherObject.gameObject;
+                navMesh.SetDestination(otherObject.transform.position);
+
+            }
+            else
+            {
+                target = null;
+                isSuspect = false;
+                if (transform.position != startingPoint)
+                {
+
+                    navMesh.stoppingDistance = 1;
+                    navMesh.SetDestination(startingPoint);
+                    if (navMesh.remainingDistance <= 1)
+                    {
+                        enemyAnimator.SetBool("walk", false);
+                        transform.rotation = Quaternion.Euler(0, 180, 0);
+                    }
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fireDistance);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, SuspectDistance);
     }
 
     void Update()
